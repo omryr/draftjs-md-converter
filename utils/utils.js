9768414a2e71923c454a53e4faa8ee0af8d1cace
omryr <omry@localcoin.com>
2019-05-06 14:00:09 +0300

    update

--- utils/utils.js
+++ utils/utils.js
@@ -22,8 +22,8 @@ function getId(url, provider) {
   // /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
   const match = url.match(regExp);
 
-  if (match && match[2].length === 11) {
-    return match[2];
+  if (match && match[1].length === 11) {
+    return match[1];
   }
   return 'error';
 }
