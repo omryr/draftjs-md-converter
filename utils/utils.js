f4be95f75e6874936eb101bf19f019a47e7bc0fa
omryr <omry@localcoin.com>
2019-05-06 13:52:40 +0300

    update

--- utils/utils.js
+++ utils/utils.js
@@ -1,5 +1,23 @@
-function getId(url) {
-  const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+const vimReg = /(http|https)?:\/\/(www\.|player\.)?vimeo\.com\/(?:channels\/(?:\w+\/)?|groups\/([^\/]*)\/videos\/|video\/|)(\d+)(?:|\/\?)/;
+const ytReg = /(?:https?:\/\/)?(?:www\.)?youtu\.?be(?:\.com)?\/?.*(?:watch|embed)?(?:.*v=|v\/|\/)([\w\-_]+)\&?/;
+
+function getEmbedProviderUrl(provider) {
+  if (provider === 'youtube') return 'https://www.youtube.com/embed/';
+  if (provider === 'vimeo') return 'https://player.vimeo.com/video/';
+  return null;
+}
+
+function getEmbedProvider(string) {
+  if (ytReg.test(string)) return 'youtube';
+  // return 'https://www.youtube.com/embed/'
+  if (vimReg.test(string)) return 'vimeo';
+  // return 'https://player.vimeo.com/video/'
+  return null;
+}
+
+function getId(url, provider) {
+  const regExp = provider === 'youtube' ? ytReg : vimReg;
+  // /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
   const match = url.match(regExp);
 
   if (match && match[2].length === 11) {
@@ -9,16 +27,15 @@ function getId(url) {
 }
 
 function buildEmbeddedUrl(string) {
-  const userUrl = string.match(
-    /(?:https?:\/\/)?(?:www\.)?youtu\.?be(?:\.com)?\/?.*(?:watch|embed)?(?:.*v=|v\/|\/)([\w\-_]+)\&?/
-  )[0];
+  const provider = getEmbedProvider(string);
+  const userUrl = string.match(ytReg)[0];
 
-  const id = getId(userUrl);
-  return `https://www.youtube.com/embed/${id}`;
+  const id = getId(userUrl, provider);
+  return `${getEmbedProviderUrl(provider)}/${id}`;
 }
 
 function testEmbed(string) {
-  const reg = /(?:https?:\/\/)?(?:www\.)?youtu\.?be(?:\.com)?\/?.*(?:watch|embed)?(?:.*v=|v\/|\/)([\w\-_]+)\&?/;
+  const reg = ytReg;
   return reg.test(string);
 }
 
